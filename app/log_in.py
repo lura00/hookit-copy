f16ee6136a6cc54d8288314d271f9882f0d85cff
lura00 <ludvig.skinstad@gmail.com>
2022-05-19 08:48:05 +0200

    cleaning code to linter

--- app/log_in.py
+++ app/log_in.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Entry
+from tkinter import Tk, Label, Entry, W
 from tkinter import messagebox
 import database
 
@@ -8,15 +8,15 @@ window = Tk()
 # Log in labels
 
 
-def getInput():
-    a = username.get()
-    b = password.get()
-    window.destroy()
+# def getInput():
+#     a = username.get()
+#     b = password.get()
+#     window.destroy()
 
-    global params
-    params = [a,b]
-    print(params)
-    return params
+#     global params
+#     params = [a,b]
+#     print(params)
+#     return params
 
 
 def saveToDb():
@@ -32,6 +32,7 @@ def saveToDb():
     database.add_user(connection, a, b, c,d, e, f, g)
     messagebox.showinfo(title="New user", message="Successfully added a new user")
     window.destroy()
+
 
 def create_user():
     Label(window, text="Enter your name: ").grid(row=0, sticky=W)
@@ -65,27 +66,27 @@ def create_user():
 
     # Button(window, text="Create & save", command=saveToDb).grid(row=7, sticky=W)
 
-def login():
+# def login():
 
-    Label(window, text="Enter username: ").grid(row=0, sticky=W)
-    Label(window, text="Enter password: ").grid(row=1, sticky=W)
-    username = Entry(window)
-    password = Entry(window)
-    username.grid(row=0, column=1)
-    password.grid(row=1, column=1)
-    connection = database.connect()
-    cur = connection.cursor()
-    uname_auth = username.get()
-    pswd_auth = password.get()
+#     Label(window, text="Enter username: ").grid(row=0, sticky=W)
+#     Label(window, text="Enter password: ").grid(row=1, sticky=W)
+#     username = Entry(window)
+#     password = Entry(window)
+#     username.grid(row=0, column=1)
+#     password.grid(row=1, column=1)
+#     connection = database.connect()
+#     cur = connection.cursor()
+#     uname_auth = username.get()
+#     pswd_auth = password.get()
 
-    authentication = (uname_auth, pswd_auth)
-    cur.execute("SELECT * FROM users;")
-    all_users = cur.fetchall()
+#     authentication = (uname_auth, pswd_auth)
+#     cur.execute("SELECT * FROM users;")
+#     all_users = cur.fetchall()
     
-    if authentication in all_users:
-        messagebox.showinfo(title="Welcome", message=uname_auth)
-    else:
-        messagebox.showerror(title="ACCESS DENIED", message="Have you forgotten your password?")
+#     if authentication in all_users:
+#         messagebox.showinfo(title="Welcome", message=uname_auth)
+#     else:
+#         messagebox.showerror(title="ACCESS DENIED", message="Have you forgotten your password?")
 
 # Button(window, text = "Login", command=login).grid(row=8, sticky=W)
 # Button(window, text = "Create new user", command=create_user).grid(row=9, sticky=W)
